0cb461f70a95ff811be8623c8aea58ae555c7ce7
Wilton Souza <willsouzafilho@gmail.com>
2021-11-30 22:42:01 -0300

    Removing comments

--- src/functions/createTodoItem.ts
+++ src/functions/createTodoItem.ts
@@ -23,16 +23,6 @@ export const handle: APIGatewayProxyHandler = async (event) => {
   const { id } = event.pathParameters;
   const { title, deadline } = JSON.parse(event.body) as ICreateTodo;
 
-  // const response = await document.query({
-  //   TableName: "users_todos",
-  //   KeyConditionExpression: "id = :id",
-  //   ExpressionAttributeValues: {
-  //     ":id": id
-  //   }
-  // }).promise();
-
-  console.log(deadline)
-
   const todoData: ITodo = {
     id: uuidV4(),
     user_id: id,
